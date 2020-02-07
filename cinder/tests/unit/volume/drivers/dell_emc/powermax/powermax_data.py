1e11782643aafe5c4e7c4bd63d5d816738ab2057
Helen Walsh <helen.walsh@emc.com>
2020-02-07 15:01:48 +0000

    PowerMax Driver - Safeguarding retype to some in-use replicated modes
    Host assisted migration is a better option for retyping from a non-replicated
    to a replicated (Asynchronous and Metro) modes.
    
    1. Block in-use non-replicated to replicated(Metro and Asynchronous)
    2. Remove any unused code
    3. Differentiate between the production and the management storage groups
    4. Remove rollback code on a cleanup_lun, its not necessary on delete and
        will not always work
    5. Allow for a target device not to exist on the target array
    6. Fix minor issue on metadata on a retype
    
    Change-Id: I6c7c94528ee94183c1df21c25388505b28f1781a
    (cherry picked from commit 92aeec3ad9dbb0482c2531a0b42c06326ae945fd)

--- cinder/tests/unit/volume/drivers/dell_emc/powermax/powermax_data.py
+++ cinder/tests/unit/volume/drivers/dell_emc/powermax/powermax_data.py
@@ -250,7 +250,7 @@ class PowerMaxData(object):
 
     test_volume_attachment = volume_attachment.VolumeAttachment(
         id='2b06255d-f5f0-4520-a953-b029196add6b', volume_id=test_volume.id,
-        connector=connector)
+        connector=connector, attached_host='HostX')
 
     location_info = {'location_info': '000197800123#SRP_1#Diamond#DSS',
                      'storage_protocol': 'FC'}
@@ -645,14 +645,16 @@ class PowerMaxData(object):
                              'snapvx_source': 'false',
                              'storageGroupId': []}
 
-    volume_details_legacy = {'cap_gb': 2,
-                             'num_of_storage_groups': 1,
-                             'volumeId': device_id,
-                             'volume_identifier': test_volume.id,
-                             'wwn': volume_wwn,
-                             'snapvx_target': 'false',
-                             'snapvx_source': 'false',
-                             'storageGroupId': []}
+    volume_details_attached_async = (
+        {'cap_gb': 2,
+         'num_of_storage_groups': 1,
+         'volumeId': device_id,
+         'volume_identifier': 'OS-%s' % test_volume.id,
+         'wwn': volume_wwn,
+         'snapvx_target': 'false',
+         'snapvx_source': 'false',
+         'storageGroupId': [
+             rdf_managed_async_grp, storagegroup_name_f + '-RA']})
 
     volume_list = [
         {'id': '6b70de13-98c5-46b2-8f24-e4e96a8988fa',
@@ -1253,3 +1255,18 @@ class PowerMaxData(object):
 
     volume_metadata = {
         'DeviceID': device_id, 'ArrayID': array, 'ArrayModel': array_model}
+
+    # retype metadata dict
+    retype_metadata_dict = {
+        'device_id': device_id,
+        'rdf_group_no': '10',
+        'remote_array': remote_array,
+        'target_device_id': device_id,
+        'rep_mode': 'Asynchronous',
+        'replication_status': 'enabled',
+        'target_array_model': array_model}
+
+    retype_metadata_dict2 = {
+        'default_sg_name': 'default-sg',
+        'service_level': 'Diamond'
+    }
